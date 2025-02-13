e2787da6f0b96937020a264c3d1e2c4f25eebea9
hindinisikc <doubleflusk@gmail.com>
2025-02-14 01:22:15 +0800

    takes two numbers and returns their product

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
-function add7() {
-    let number = prompt("give me any number and I'd add it by 7");
-    let add = parseInt(number) + parseInt(7)
+// Takes one number and returns that number +7
 
-    alert(add)
+// function add7() {
+//     let number = prompt("give me any number and I'd add it by 7");
+//     let add = parseInt(number) + parseInt(7)
+
+//     alert(add)
+// }
+
+// add7()
+
+function multiply() {
+    const a = prompt("give me a number");
+    const b = prompt("another number");
+
+    const product = (a, b) => a * b;
+
+    alert(product(a, b) );
+
 }
 
-add7()
+multiply();
